cbd736c0f53020fe4c2a99030ab9743e8ab52017
ganeshallampalli <ganeshallampalli@gmail.com>
2019-06-15 23:38:51 +0530

    Added cruise module

--- src/app/shared/components/cruise/cruise.component.ts
+++ src/app/shared/components/cruise/cruise.component.ts
@@ -1,15 +1,105 @@
 import {Component, OnInit} from '@angular/core';
+import {MatDialog, MatSnackBar} from "@angular/material";
+import {FormComponent} from "../form/form.component";
+import {Experience} from "../../models/experience";
+import {ExperienceService} from "../../services/experience.service";
 
 @Component({
-  selector: 'kiel-cruise',
-  templateUrl: './cruise.component.html',
-  styleUrls: ['./cruise.component.scss']
+    selector: 'kiel-cruise',
+    templateUrl: './cruise.component.html',
+    styleUrls: ['./cruise.component.scss']
 })
 export class CruiseComponent implements OnInit {
 
-  constructor() { }
+    constructor(private dialog: MatDialog,
+                private cruiseService: ExperienceService,
+                private snackBar: MatSnackBar) {
+    }
 
-  ngOnInit() {
-  }
+    cruiseList: Experience[] = [
+        {
+            name: "Dog Breed",
+            description: "   The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from\n" +
+                "                            Japan.\n" +
+                "                            A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was\n" +
+                "                            originally\n" +
+                "                            bred for hunting."
+        },
+        {
+            name: "Dog Breed",
+            description: "   The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from\n" +
+                "                            Japan.\n" +
+                "                            A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was\n" +
+                "                            originally\n" +
+                "                            bred for hunting."
+        },
+        {
+            name: "Dog Breed",
+            description: "   The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from\n" +
+                "                            Japan.\n" +
+                "                            A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was\n" +
+                "                            originally\n" +
+                "                            bred for hunting."
+        }, {
+            name: "Dog Breed",
+            description: "   The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from\n" +
+                "                            Japan.\n" +
+                "                            A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was\n" +
+                "                            originally\n" +
+                "                            bred for hunting."
+        },
+        {
+            name: "Dog Breed",
+            description: "   The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from\n" +
+                "                            Japan.\n" +
+                "                            A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was\n" +
+                "                            originally\n" +
+                "                            bred for hunting."
+        }, {
+            name: "Dog Breed",
+            description: "   The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from\n" +
+                "                            Japan.\n" +
+                "                            A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was\n" +
+                "                            originally\n" +
+                "                            bred for hunting."
+        }, {
+            name: "Dog Breed",
+            description: "   The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from\n" +
+                "                            Japan.\n" +
+                "                            A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was\n" +
+                "                            originally\n" +
+                "                            bred for hunting."
+        },
 
+
+    ]
+
+    openForm(): void {
+        const dialogRef = this.dialog.open(FormComponent, {
+            data: {name: "Experience"},
+            width: "500px"
+        });
+
+        dialogRef.afterClosed().subscribe(result => {
+            console.log('The dialog was closed');
+        });
+    }
+
+    ngOnInit() {
+        this.cruiseService.getExperiences("cruise").subscribe(response => {
+
+            if (response.code === "200") {
+                this.cruiseList = response.cruises;
+                this.closeForm();
+            }
+        }, () => {
+            this.snackBar.open("There exists a problem while connecting to the server. Sorry for the inconvenience", "500", {
+                duration: 5000
+            });
+        });
+    }
+
+    closeForm() {
+        this.dialog.closeAll();
+    }
 }
--- src/app/shared/components/cruise/cruise.module.ts
+++ src/app/shared/components/cruise/cruise.module.ts
@@ -1,11 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CruiseComponent} from "./cruise.component";
+import {MaterialModule} from "../../../material.module";
+import {FormModule} from "../form/form.module";
 
 @NgModule({
-  declarations: [CruiseComponent],
-  imports: [
-    CommonModule
-  ]
+    declarations: [CruiseComponent],
+    imports: [
+        CommonModule,
+        MaterialModule,
+        FormModule,
+    ]
 })
-export class CruiseModule { }
+export class CruiseModule {
+}
