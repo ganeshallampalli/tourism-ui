7b544eff1bf83bba34e42ef9d86ad9fde66d8185
ganeshallampalli <ganeshallampalli@gmail.com>
2019-06-17 22:29:36 +0530

    Added edit and delete actions for cruise card

--- src/app/shared/components/cruise/cruise.component.ts
+++ src/app/shared/components/cruise/cruise.component.ts
@@ -70,13 +70,43 @@ export class CruiseComponent implements OnInit {
                 "                            originally\n" +
                 "                            bred for hunting."
         },
+        {
+            name: "Dog Breed",
+            description: "   The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from\n" +
+                "                            Japan.\n" +
+                "                            A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was\n" +
+                "                            originally\n" +
+                "                            bred for hunting."
+        }, {
+            name: "Dog Breed",
+            description: "   The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from\n" +
+                "                            Japan.\n" +
+                "                            A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was\n" +
+                "                            originally\n" +
+                "                            bred for hunting."
+        },
+        {
+            name: "Dog Breed",
+            description: "   The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from\n" +
+                "                            Japan.\n" +
+                "                            A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was\n" +
+                "                            originally\n" +
+                "                            bred for hunting."
+        }, {
+            name: "Dog Breed",
+            description: "   The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from\n" +
+                "                            Japan.\n" +
+                "                            A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was\n" +
+                "                            originally\n" +
+                "                            bred for hunting."
+        },
 
 
     ]
 
     openForm(): void {
         const dialogRef = this.dialog.open(FormComponent, {
-            data: {name: "Experience"},
+            data: {name: "Cruise"},
             width: "500px"
         });
 
@@ -101,5 +131,24 @@ export class CruiseComponent implements OnInit {
 
     closeForm() {
         this.dialog.closeAll();
+    }
+
+    get isAdmin() {
+        return localStorage.getItem("role") === "ADMIN";
+    }
+
+    deleteCruise(cruiseId) {
+        this.cruiseService.deleteExperience("cruise", cruiseId).subscribe(response => {
+            if (response.code === 200) {
+                this.snackBar.open(response.message, response.code, {
+                    duration: 5000
+                });
+                this.cruiseService.getExperiences("cruise").subscribe(response => this.cruiseList = response.cruises);
+            }
+        }, () => {
+            this.snackBar.open("There exists a problem while connecting to the server. Sorry for the inconvenience", "500", {
+                duration: 5000
+            });
+        });
     }
 }
