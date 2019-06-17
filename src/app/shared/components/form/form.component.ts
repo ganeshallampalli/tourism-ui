7d7d7e111f08b2e74e3d87e5d553684b8cce9e93
ganeshallampalli <ganeshallampalli@gmail.com>
2019-06-17 22:33:48 +0530

    Modified the form with two more conditional fields

--- src/app/shared/components/form/form.component.ts
+++ src/app/shared/components/form/form.component.ts
@@ -5,57 +5,68 @@ import {Experience} from "../../models/experience";
 import {ExperienceService} from "../../services/experience.service";
 
 @Component({
-  selector: 'kiel-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+    selector: 'kiel-form',
+    templateUrl: './form.component.html',
+    styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
 
 
-  form: FormGroup;
-  encodedImage = "";
+    form: FormGroup;
+    encodedImage = "";
 
-  constructor(private fb: FormBuilder,
-              @Inject(MAT_DIALOG_DATA) public data,
-              private snackBar: MatSnackBar,
-              private cruiseService: ExperienceService) { }
-
-  ngOnInit() {
-    this.createForm();
-  }
-
-  getImage(encodedImage) {
-    this.encodedImage = encodedImage;
-  }
-
-
-  createForm() {
-    this.form = this.fb.group({
-      name: new FormControl("", [Validators.required]),
-      description: new FormControl("", [Validators.required])
-    })
-  }
-
-  submitForm() {
-
-    const cruise: Experience = {
-      name: this.form.controls["name"].value,
-      description: this.form.controls["description"].value,
-      encodedImage: this.encodedImage
-    };
-
-    if(this.form.valid) {
-      if(this.data.name === "Experience") {
-        this.cruiseService.addExperience("cruise" ,cruise).subscribe(res => {
-          this.snackBar.open(res.message, res.code, {
-            duration: 5000
-          })
-        }, () => {
-          this.snackBar.open("There exists a problem while connecting to the server", "500", {
-            duration: 5000
-          });
-        });
-      }
+    constructor(private fb: FormBuilder,
+                @Inject(MAT_DIALOG_DATA) public data,
+                private snackBar: MatSnackBar,
+                private experienceService: ExperienceService) {
     }
-  }
+
+    ngOnInit() {
+        this.createForm();
+    }
+
+    getImage(encodedImage) {
+        this.encodedImage = encodedImage;
+    }
+
+
+    createForm() {
+        this.form = this.fb.group({
+            name: new FormControl("", [Validators.required]),
+            description: new FormControl("", [Validators.required]),
+            cost: new FormControl("", []),
+            avgTimeSpent: new FormControl("", [])
+        })
+    }
+
+    submitForm() {
+
+        const experience: Experience = {
+            name: this.form.controls["name"].value,
+            description: this.form.controls["description"].value,
+            encodedImage: this.encodedImage
+        };
+
+        let type = "";
+        if (this.form.valid) {
+           if (this.data.name === "Cuisine") {
+                experience.cost = this.form.controls["cost"].value
+            }
+
+            if (this.data.name === "Things to do") {
+                experience.cost = this.form.controls["avgTimeSpent"].value
+            }
+
+            this.experienceService.addExperience(this.data.name.toLowerCase(), experience).subscribe(res => {
+                this.snackBar.open(res.message, res.code, {
+                    duration: 5000
+                })
+            }, () => {
+                this.snackBar.open("There exists a problem while connecting to the server", "500", {
+                    duration: 5000
+                });
+            });
+
+        }
+    }
 }
